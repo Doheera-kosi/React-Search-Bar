8527818f2b32033c9677714fe71ae7e3148ccec0
Doheera-kosi <kupourevans99@gmail.com>
2023-04-09 07:12:37 +0000

    Add search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
     <div className="App">
       <div className="search-bar-container">
-        <div>SearchBar</div>
+        <SearchBar />
         <div>SearchResults</div>
       </div>
     </div>
