a9c33bca50ca270d24b23ce2b2a3d2b821456a06
Doheera-kosi <kupourevans99@gmail.com>
2023-04-09 07:23:10 +0000

    Add searchbar styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
+import "./SearchBar.css"
 
 const SearchBar = () => {
   return (
     <div className='input-wrapper'>
       <FaSearch id='search-icon'/>
+      <input type="text" name="" id="" placeholder='Type to search...'/>
     </div>
   )
 }
